060bbe67efd22ab6fa9b1fe6c103f5455e21f732
Satya Ranjan Padhiary <67493681+Since1997-web@users.noreply.github.com>
2023-04-16 10:02:21 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 #import requests
 #creating a function
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     #streamlit.text(fruityvice_response.json())
     # Below mentioned line of code used to normalize the json data into a tabular fromat
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
