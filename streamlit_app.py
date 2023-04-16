53d34a75d7a6ea281722f1245e433cdf106ceb26
Satya Ranjan Padhiary <67493681+Since1997-web@users.noreply.github.com>
2023-04-16 09:46:36 +0530

    Update streamlit_app.py
    try and except implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,19 @@ streamlit.dataframe(fruits_to_show)
 
 #import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# Below mentioned line of code used to normalize the json data into a tabular fromat
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displaying the normalized data inside the app
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit choice:
+    streamlit.error("Please select a fruit to get information.")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # Below mentioned line of code used to normalize the json data into a tabular fromat
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # displaying the normalized data inside the app
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
 #adding stop method
 streamlit.stop()
