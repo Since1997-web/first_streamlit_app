ab3da45069a84537f8275945671c98148306b5af
Satya Ranjan Padhiary <67493681+Since1997-web@users.noreply.github.com>
2023-04-16 09:36:57 +0530

    Update streamlit_app.py
    organizing imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title("My parents new healthy diner")
 
 streamlit.header("Breakfast Favorites")
@@ -9,7 +14,7 @@ streamlit.text("🥑🍞  Hard-Boiled Free-Range Egg")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -17,7 +22,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-import requests
+#import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -32,7 +37,7 @@ streamlit.dataframe(fruityvice_normalized)
 #adding stop method
 streamlit.stop()
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
